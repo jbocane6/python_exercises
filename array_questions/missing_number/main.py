bafde441f344d2f6dc6d61e902e96b33de39a269
Juan Bocanegra <jbocane6@gmail.com>
2022-09-30 22:12:36 -0500

    [UPDATE] adding test

--- array_questions/missing_number/main.py
+++ array_questions/missing_number/main.py
@@ -8,11 +8,11 @@ def main():
         miss = random.randrange(1, n)
         print(f"The number to remove is: {miss}")
         list = [x for x in range(1, n+1)]
-        m.missing_number([y for y in list if y != miss])
+        print(m.missing_number([y for y in list if y != miss]))
 
-    m.missing_number([1, 2, True])
-    m.missing_number([1, 2, ""])
-    m.missing_number([1, 2, []])
+    print(m.missing_number([1, 2, True]))
+    print(m.missing_number([1, 2, ""]))
+    print(m.missing_number([1, 2, []]))
 
 
 if __name__ == "__main__":
--- array_questions/missing_number/missing_number.py
+++ array_questions/missing_number/missing_number.py
@@ -6,12 +6,13 @@ if __name__ != "__main__":
     def missing_number(nList):
         if validate(nList):
             miss = ((len(nList)+1) * (len(nList) + 2) // 2)-sum(nList)
-            print(f"The missing number is: {miss}")
+            return f"The missing number is: {miss}"
+        else:
+            return f"Error, there are not int values in list"
 
     # check if all items are int
     def validate(nList):
         for i in nList:
             if not isinstance(i, int) or isinstance(i, bool):
-                print(f"Error, there are not int values in list")
                 return False
         return True
